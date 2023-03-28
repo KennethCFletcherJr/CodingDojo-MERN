ab7beb53b53244e275a08b736dd483c57450df7f
fletc <fletcher.kenneth@gmail.com>
2023-03-28 13:53:11 -0500

    Updated ToDoList.js delete

--- week3/ToDoList/todolist/src/App.js
+++ week3/ToDoList/todolist/src/App.js
@@ -6,45 +6,138 @@ function App() {
 
   const [newTask, setNewTask] = useState("");
   //Empty arrays won't break if you try to map them using dot operator.
-  const [list, setNewListItem] = useState([]);
+  const [list, taskItem] = useState([]);
+
+  //setNewListItem
+
   //Is empty array, starting, default, or updated? You can mark it null to distiguish the starging arr, but you will have to do a condition, "...if it is null, don't try to map it, render it instead (render loading, or something)..."
 
-  const handleNewTaskSubmit = (e) => {
-
-    e.prevent.Default()
+  const handleNewTaskSubmit = (event) => {
+    event.prevent.Default()
     // console.log(newTask)
     // list.push(newTask); 
-    setNewListItem([...list, newTask]);
-    // setNewTask("");
+    if (newTask.length = 0) {
+      return;
+    }
+    const taskItem() = {
+      text: newTask,
+      complete: false
+    }
+    setNewListItem([...list, taskItem]);
+    setNewTask("");
   }
 
-  return (
-    <div className="App">
+  const handleNewTaskDelete(delIdx) {
+    const filteredList = list.filter((newTask, i) => {
+      return i !== delIdx;
+    });
 
-      <form onSubmit={(event) => {
-        handleNewTaskSubmit(event);
-        //list.push(newList); Mutating the state directly results in unpredictible 
-        //asynchronous behavior.
+    setList = (filteredList);
+  };
 
-      }}>
-        <input onChange= {(event) => {
-          setNewTask(event.target.value);
-        }}
+  const handleToggleComplete = (idx) => {
 
-          type="text" value={newTask}/>
+    const updatedList = {
+      list.map((task, i) => {
+        if (idx === 0) {
+          task.complete = !task.complete;
+          // const updatedTask = { ...list, complete: !newTask.complete };
+          // return updatedTask;
+        }
+        return task;
+      });
 
-        <div>
+      setList(updatedList);
+    }
 
-          <button type="button">Add</button>
+  }
+
+
+
+  // if (idx === i) {}
+
+  // newTask.complete = !newTask.complete; 
+
+  // // const updatedTask = { ...list, complete: !newTask.complete }; 
+  // // return updatedTask;
+
+  // return task;
+
+  // set
+
+
+
+
+}
+
+return (
+  <div className="App">
+
+    <form onSubmit={(event) => {
+      handleNewTaskSubmit(event);
+      //list.push(newList); Mutating the state directly results in unpredictible 
+      //asynchronous behavior.
+
+    }}>
+      <input onChange={(event) => {
+        setNewTask(event.target.value);
+      }}
+
+        type="text" value={newTask} />
+
+      <div>
+
+        <button type="button">Add</button>
+
+      </div>
+
+    </form>
+
+    {list.map((newTask, i) => {
+
+      const taskClasses = ["bold", "italic"];
+
+      // const taskClasses = [];
+
+      if (task.complete) {
+
+        taskClasses.push("line-through");
+
+      }
+
+      return (
+
+        <div key={i}>
+
+          <input onChange={(event) => {
+
+            handleToggleComplete(i);
+
+          }}
+
+            checked="newTask.complete"=false  type="checkbox"/>
+
+
+          <span className={taskClasses.join("")}>{newTask.text}</span>
+
+
+          <button onClick={(delIdx) => {
+
+            handleNewTaskDelete(i);
+
+          }}>Delete</button>
 
         </div>
 
-      </form>
+      );
 
-    </div>
 
-  );
-}
+    })}
+
+    );
+
+  </div>
+
 
 export default App;
 
@@ -56,7 +149,5 @@ export default App;
 // const newTaskStateArr("useState");
 // const newTask = newTaskStateArr[0];
 // const setNewTask = newTaskStateArr[1];
-
-
 
 
