0dcdf89336cf5e64814cf3012db41c8ba3b05c43
fletc <fletcher.kenneth@gmail.com>
2023-04-04 21:22:15 -0500

    Update again

--- week4/HelloExpress/server.js
+++ week4/HelloExpress/server.js
@@ -4,9 +4,17 @@ const port = 8000;
 
 // req is short for request
 // res is short for response
+//WHAT'S GOING ON HERE??? (Reating Routes and sending data???)
+//The app instance has various HTTP verb methods that we can call, for example the get("/api" , )
+
+//HTTP verb methods =  GET requests at this particular route (n"/api").
+//The second argument passed to the get method is the callback function we want to run when a browser makes a request to this route.
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello World" });
+  //Getting a local /api folder, passing a req, res, procedure....accessing json from the res, namely the message: "Hello World"???
+  res.json({ message: "My, Oh My!" });
 });
 
 // this needs to be below the other code blocks
+//the app.listen() line of code that actually runs our server on a specified port.
+
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
--- week4/routing-practice/src/App.js
+++ week4/routing-practice/src/App.js
@@ -1,76 +1,28 @@
-import React from "react";
-import {
-  BrowserRouter,
-  Link,
-  Routes,
-  Route
-} from "react-router-dom";
-import { useParams } from 'react-router-dom';
-
-const Greeting = (props) => {
-
-  const { page } = useParams();
-
-  return (
-    <h1>{page}!</h1>
-  );
-}
+import './App.css';
+import Home from './components/Home';
+import ParamsComponent from './components/ParamsComponent.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-
-      <p className="App">
-        <Link to="/page/Welcome!">Welcome!</Link>
-        |
-        <Link to="/page/4">4</Link>
-        |
-        <Link to="/page/hello">hello</Link>
-        |
-        <Link to="/page/{hello}">hello</Link>
-
-      </p>
-
-      <Routes>
-
-        <Route element={<Greeting />} path="/page/:Greeting"  />
-
-        {/* /home is a "hard path". There aren't params in the route. */}
-        <Route element={<Home />} path="/home" />
-        {/* We have two potential routes/paths  for the ParamsComponent */}
-        <Route element={<ParamsComponent />} path="/:word" />
-        {/* These values (params) can be destructured from the useParam hook and used as variables, 
+      <div className="App">
+      
+        <Routes>
+          {/* /home is a "hard path". There aren't params in the route. */}
+          <Route element={<Home />} path="/home" />
+          {/* We have two potential routes/paths  for the ParamsComponent */}
+          <Route element={<ParamsComponent />} path="/:word" />
+          {/* These values (params) can be destructured from the useParam hook and used as variables, 
         just like values destructured from props! */}
-        <Route element={<ParamsComponent />} path="/:word/:color/:bgCol" />
+          <Route element={<ParamsComponent />} path="/:word/:color/:bgCol" />
+        </Routes>
 
-
-      </Routes>
+      </div>
     </BrowserRouter>
-
   );
 }
 
-
 export default App;
 
-// Create a simple React app that can handle the following routes:
-
-// 1. localhost: 3000 / home: This should display the home page, with a generic "Welcome" heading
-
-// 2. localhost: 3000 / 4: This should display the number "4", or any other number that was sent through
-
-// 3. localhost: 3000 / hello: This should display the word "hello" or any other word that was sent through
-
-// 4. localhost: 3000 / hello / blue / red: This should display the word "hello" in blue text with a red background.It should work with any other valid word and colors.
-
-//   Hint: use the isNaN method(is Not a Number).For example: isNaN(+"35") is false, isNaN(+"hello") is true
-
-        // <Route path="/page/:Welcome!" element={<Greeting />} />
-        // <Route path="/page/:4 element" element={<Greeting />} />
-        // <Route path="/page/:greeting" element={<Greeting />} />
-        // <Route path="/page/:greeting" element={<Greeting />} />
-
-
-
-         // <Route path="/page/:Greeting" element={<Greeting />} />
-
+//The Route contains the element, equal to component, and the path, divided by forward slashes and colon activating the following attribute/property
