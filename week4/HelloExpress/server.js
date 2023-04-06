191c06213d1c260805052028177c1af137023353
fletc <fletcher.kenneth@gmail.com>
2023-04-06 02:39:41 -0500

    Update

--- week4/HelloExpress/server.js
+++ week4/HelloExpress/server.js
@@ -4,10 +4,10 @@ const port = 8000;
 
 // req is short for request
 // res is short for response
-//WHAT'S GOING ON HERE??? (Reating Routes and sending data???)
+//WHAT'S GOING ON HERE??? (Reading Routes and sending data???)
 //The app instance has various HTTP verb methods that we can call, for example the get("/api" , )
 
-//HTTP verb methods =  GET requests at this particular route (n"/api").
+//HTTP verb methods =  GET requests at this particular route ("/api").
 //The second argument passed to the get method is the callback function we want to run when a browser makes a request to this route.
 app.get("/api", (req, res) => {
   //Getting a local /api folder, passing a req, res, procedure....accessing json from the res, namely the message: "Hello World"???
