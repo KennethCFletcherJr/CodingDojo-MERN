13703a7f80de27b1bf9020ae98682b2d9c3a9b9b
fletc <fletcher.kenneth@gmail.com>
2023-04-09 16:23:29 -0500

    Update After Node_Module Delete and Reinstall

---
+++ week4/expressproject/server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// req is shorthand for request
+// res is shorthand for response
+
+//! List of instructions:
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello World" });
+});
+
+
+// this needs to be below the other code blocks
+app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+
+
